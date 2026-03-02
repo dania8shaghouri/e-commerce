990f67cdf5ea8c992bcef8b8762e965fa22d3aae
dania <daniashaghouri@gmail.com>
2026-03-02 16:28:04 +0300

    product feature

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 import userRoute from "./routes/userRoutes.js";
+import { seedInitialProducts } from "./services/productService.js";
+import productRoute from "./routes/productRoutes.js";
 dotenv.config();
 
 const app = express();
 const port = 3001;
 
-app.use(express.json())
+app.use(express.json());
 
 const MONGODB_URI = process.env.MONGODB_URI as string;
 mongoose
@@ -16,7 +18,11 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
+// seed the products to database
+seedInitialProducts();
+
 app.use("/user", userRoute);
+app.use("/product", productRoute);
 
 app.listen(port, () => {
   console.log("running on port " + port);
