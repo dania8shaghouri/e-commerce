8f0ae11f64df6ce6c92decb006ebcb1495f4727d
dania <daniashaghouri@gmail.com>
2026-04-14 15:43:29 +0300

    added cartcontext and cart provider

---
+++ frontend/src/context/Auth/cart/CartContext.ts
@@ -0,0 +1,15 @@
+import { useContext, createContext } from "react";
+import type { CartItem } from "../../../types/CartItem";
+
+interface CartContextType {
+  cartItems: CartItem[];
+  totalAmount: number;
+  addItemToCart: (ProductId: number) => void;
+}
+export const CartContext = createContext<CartContextType>({
+  cartItems: [],
+  totalAmount: 0,
+  addItemToCart: () => {},
+});
+
+export const useCart = () => useContext(CartContext);
