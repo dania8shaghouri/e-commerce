8f3e0d0edb6e387c5cc2b94d3753a5cb5cbecd92
dania <daniashaghouri@gmail.com>
2026-06-17 22:08:56 +0300

    feat(admin-dashboard) add recent orders table

--- frontend/src/features/admin/pages/admin/DashboardPage.tsx
+++ frontend/src/features/admin/pages/admin/DashboardPage.tsx
@@ -1,6 +1,6 @@
 import StatsGrid from "../../components/dashboard/StatsGrid.jsx";
 import RevenueChart from "../../components/dashboard/RevenueChart.jsx";
-// import RecentOrdersTable from "../../components/dashboard/RecentOrdersTable";
+import RecentOrdersTable from "../../components/dashboard/RecentOrdersTable";
 // import TopProducts from "../../components/dashboard/TopProducts";
 // import OrderStatusChart from "../../components/dashboard/OrderStatusChart";
 
@@ -15,9 +15,19 @@ const DashboardPage = () => {
         <div>{/* <OrderStatusChart /> */}</div>
       </div>
 
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-        {/* <RecentOrdersTable />
-        <TopProducts /> */}
+      {/* <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+        {
+          <RecentOrdersTable />
+          // <TopProducts />
+        }
+      </div> */}
+
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+        <div className="lg:col-span-2">
+          <RecentOrdersTable />
+        </div>
+
+        <div className="lg:col-span-1">{/* Top Products */}</div>
       </div>
     </div>
   );
