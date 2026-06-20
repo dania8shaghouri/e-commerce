8807272a0d21ea31891b6d8ef6b5a82b3f0739bb
dania <daniashaghouri@gmail.com>
2026-06-20 08:44:01 +0300

    feat(admin-dashboard) add order status chart  and Recharts

--- frontend/src/features/admin/pages/admin/DashboardPage.tsx
+++ frontend/src/features/admin/pages/admin/DashboardPage.tsx
@@ -2,7 +2,7 @@ import StatsGrid from "../../components/dashboard/StatsGrid.js";
 import RevenueChart from "../../components/dashboard/RevenueChart.js";
 import RecentOrdersTable from "../../components/dashboard/RecentOrdersTable.js";
 import TopProducts from "../../components/dashboard/TopProducts.js";
-// import OrderStatusChart from "../../components/dashboard/OrderStatusChart";
+import OrderStatusChart from "../../components/dashboard/OrderStatusChart.js";
 
 const DashboardPage = () => {
   return (
@@ -12,13 +12,10 @@ const DashboardPage = () => {
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
         <div className="lg:col-span-2">{<RevenueChart />}</div>
 
-        <div>{/* <OrderStatusChart /> */}</div>
+        <div className="lg:col-span-1">
+          <OrderStatusChart />
+        </div>
       </div>
-
-      {/* <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-        {<RecentOrdersTable />}
-        <TopProducts />
-      </div> */}
 
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
         <div className="lg:col-span-2">
